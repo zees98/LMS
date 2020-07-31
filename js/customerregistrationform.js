5c5e8b0b7ca2bb4121e7039bb0c6bce30e2542b7
haseeb-ariff <59887124+haseeb-ariff@users.noreply.github.com>
2020-07-31 14:16:39 +0500

    changes

---
+++ js/customerregistrationform.js
@@ -0,0 +1,20 @@
+window.onload= (e)=>{
+    const file = document.querySelector('#file');
+    const img = document.querySelector('#image');
+    const cameraicon = document.querySelector('#icon');
+    
+    file.addEventListener('change',()=>{
+        Reader(file);
+    });
+    function Reader(input){
+        if(input.files && input.files[0]){
+            var a = new FileReader();
+            a.onload = (e)=>{
+                img.setAttribute("src",e.target.result);
+                cameraicon.style.display="none";
+
+            };
+            a.readAsDataURL(input.files[0]);
+        }
+    };
+}
