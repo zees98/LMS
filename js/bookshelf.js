b28110798a64699a3336c11a38a497aaa8ab00dc
Zeeshan Ali <zeeshanali@Zeeshans-iMac.local>
2020-08-03 14:02:24 +0500

    Book details added

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-    $('.book-shelf').addClass('col-lg-4 col-md-4 col-sm-6');
+    $('.book-shelf').addClass('col-lg-2 col-md-4 col-sm-6');
     $('.book-shelf').click(function () {
         // alert("Hello");
         console.log($(this).find('h5').slideToggle());
     });
     $('.book-shelf img').on('mouseenter', function () {
         $(this).addClass('book-shelf-anim');
+    });
+    $('.book-shelf img').on('mouseleave', function () {
+        $(this).removeClass('book-shelf-anim');
     });
 });
 
