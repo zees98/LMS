335ca95c6411fa732c109de975386163ed11a114
zees98 <zeeshanhamdani98@gmail.com>
2020-08-07 18:17:38 +0500

    Admin Dashboard

---
+++ js/admin/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $("li.subMenu").click(function(e) {
+
+        $("#subMenuItems").slideToggle();
+
+    });
+});
--- php/hello.php
+++ php/hello.php
@@ -1,14 +1,25 @@
 <?php
+$database =  "hariscorp_zfhlibrary";
+$conn = mysqli_connect(
+    // Hostname
+    "68.183.162.131",
+    // Username
+    "hariscorp",
+    // Password
+    "",
+    // Database
+    $database,
+    // Port
+    3306
 
-    $conn = mysqli_connect(
-        "remotemysql.com",
-        "2EeU51fxMG",
-        "qaxpb7JGoR",
-        "2EeU51fxMG",
-        3306
-        
-    );
-    echo $conn ? "Connected" : "Failed";
+);
+echo $conn ? "Connected" : "Failed";
+
+
+$queryInsertUser = 'Insert into 2EeU51fxMG.users VALUES ("", "Zeeshan98", "Zeeshan Ali", "1234")';
+
+$execInsertQuery = mysqli_query($conn, $queryInsertUser);
+
+echo $execInsertQuery;
 
 ?>
-
