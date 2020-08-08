92b0cd727fd74d6d43e9fbf3947002575db1e5a6
Fahad-khalid97 <55343805+Fahad-khalid97@users.noreply.github.com>
2020-08-08 15:31:52 -0700

    dialogbox added on issuedbooks

---
+++ js/returnbook.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+    $("#return").click(function(e) {
+        //Show and hide alert box
+        $("#dlgbx").fadeIn();
+    });
+
+    $("#confirm").click(function(e) {
+        $("#dlgbx h1").text(
+            "Success"
+        );
+
+    });
+    $("#cancel").click(function(e) {
+        $("#dlgbx").fadeOut();
+
+    });
+});
