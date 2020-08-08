89b08cf0b1e9d6f126a1f74fff699b9c7863f02b
haseeb-ariff <59887124+haseeb-ariff@users.noreply.github.com>
2020-08-09 02:53:45 +0500

    page transitions added

---
+++ js/history.js
@@ -0,0 +1,40 @@
+
+
+
+ $("#dashboard").click(function(e){
+    e.preventDefault();
+    $("body").fadeOut();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 350);
+    }
+});
+
+$("#issuedbooks").click(function(e){
+    e.preventDefault();
+    $("body").fadeOut();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 350);
+    }
+});
+
+$("#customerprofile").click(function(e){
+    e.preventDefault();
+    $("body").fadeOut();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 350);
+    }
+});
+
+
+
+
+
