5e9b2475b7ecfe92cc3e3634a65a4fc3f30c9d1e
haseeb-ariff <59887124+haseeb-ariff@users.noreply.github.com>
2020-08-10 03:36:41 +0500

    search book added

---
+++ js/searchbook.js
@@ -0,0 +1,55 @@
+$("#dashboard").click(function(e){
+    e.preventDefault();
+    $("#side").fadeOut();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 350);
+    }
+});
+
+$("#issuedbooks").click(function(e){
+    e.preventDefault();
+    $("#side").fadeOut();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 350);
+    }
+});
+
+$("#customerprofile").click(function(e){
+    e.preventDefault();
+    $("#side").fadeOut();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 350);
+    }
+});
+
+
+$("#history").click(function(e){
+    e.preventDefault();
+    $("#side").fadeOut();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 350);
+    }
+});
+
+$("#pastdue").click(function(e){
+    e.preventDefault();
+    $("#side").fadeOut();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 350);
+    }
+});
