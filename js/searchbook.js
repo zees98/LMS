bec2c30842b8c004ca00897b6b3969f2e40fbd6d
Fahad-khalid97 <55343805+Fahad-khalid97@users.noreply.github.com>
2020-08-09 17:01:17 -0700

    search screen linked to other screens

--- js/searchbook.js
+++ js/searchbook.js
@@ -1,55 +1,74 @@
-$("#dashboard").click(function(e){
+$("#dashboard").click(function(e) {
     e.preventDefault();
     $("#side").fadeOut();
     if (this.href) {
         var target = this.href;
-        setTimeout(function(){
+        setTimeout(function() {
             window.location = target;
         }, 350);
     }
 });
 
-$("#issuedbooks").click(function(e){
+$("#issuedbooks").click(function(e) {
     e.preventDefault();
     $("#side").fadeOut();
     if (this.href) {
         var target = this.href;
-        setTimeout(function(){
+        setTimeout(function() {
             window.location = target;
         }, 350);
     }
 });
 
-$("#customerprofile").click(function(e){
+$("#customerprofile").click(function(e) {
     e.preventDefault();
     $("#side").fadeOut();
     if (this.href) {
         var target = this.href;
-        setTimeout(function(){
+        setTimeout(function() {
             window.location = target;
         }, 350);
     }
 });
 
 
-$("#history").click(function(e){
+$("#history").click(function(e) {
     e.preventDefault();
     $("#side").fadeOut();
     if (this.href) {
         var target = this.href;
-        setTimeout(function(){
+        setTimeout(function() {
             window.location = target;
         }, 350);
     }
 });
 
-$("#pastdue").click(function(e){
+$("#pastdue").click(function(e) {
     e.preventDefault();
     $("#side").fadeOut();
     if (this.href) {
         var target = this.href;
-        setTimeout(function(){
+        setTimeout(function() {
             window.location = target;
         }, 350);
     }
+});
+
+$(document).ready(function() {
+
+    $("#filter").click(function(e) {
+        //Show and hide alert box
+        $("#dlgbx").fadeIn();
+    });
+
+    $("#confirm").click(function(e) {
+        $("#dlgbx h1").text(
+            "Success"
+        );
+
+    });
+    $("#cancel").click(function(e) {
+        $("#dlgbx").fadeOut();
+
+    });
 });
