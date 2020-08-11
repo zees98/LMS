5e3f4ce7fcce7f6f24f79501952baf70eb7b400f
Fahad-khalid97 <55343805+Fahad-khalid97@users.noreply.github.com>
2020-08-11 06:37:33 -0700

    done changes

--- js/contactus.js
+++ js/contactus.js
@@ -21,30 +21,30 @@ $(document).ready(function() {
 
 
 
-    // $("#click").click(function(e) {
-    //     var memb_name = $("#name").val();
-    //     var memb_email = $("#email").val();
-    //     var memb_message = $("#message").val();
-    //     var addRequest = new XMLHttpRequest();
-    //     addRequest.open("POST", "../php/feedback.php", true);
-    //     addRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    $("#click").click(function(e) {
+        var memb_name = $("#name").val();
+        var memb_email = $("#email").val();
+        var memb_message = $("#message").val();
+        var addRequest = new XMLHttpRequest();
+        addRequest.open("POST", "../php/feedback.php", true);
+        addRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-    //     addRequest.onreadystatechange = function() {
+        addRequest.onreadystatechange = function() {
 
-    //         if (this.readyState == 4 && this.status == 200) {
-    //             alert(addRequest.responseText);
+            if (this.readyState == 4 && this.status == 200) {
+                alert(addRequest.responseText);
 
-    //         } else {
-    //             alert("failed");
-    //         }
-    //     }
-    //     addRequest.send(
-    //         "Membername=" + memb_name + "&" +
-    //         "Email=" + memb_email + "&" +
-    //         "Message=" + memb_message
-    //     );
+            } else {
+                alert("failed");
+            }
+        }
+        addRequest.send(
+            "Membername=" + memb_name + "&" +
+            "Email=" + memb_email + "&" +
+            "Message=" + memb_message
+        );
 
 
-    // });
+    });
 
 });
